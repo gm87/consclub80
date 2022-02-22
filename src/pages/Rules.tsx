0e77eabbbbba311ab5cabcf9d88d1a1f71db9c93
Graham Matthews <gm87@evansville.edu>
2022-02-22 07:25:51 -0600

    rules

--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -6,9 +6,111 @@ import SidebarWithContent from "../layouts/SidebarWithContent"
 const Rules = () => {
     return (
         <SidebarWithContent>
-            <Container fluid className="bg-light h-100">
+            <Container fluid className="bg-light h-100 pe-5">
                 <Row>
-                    <Col><h1 className="display-1 fs-1 text-center mt-3">Rules</h1></Col>
+                    <Col><h1 className="display-1 fs-1 text-center mt-3 mb-5">Rules</h1></Col>
+                </Row>
+                <Row>
+                    <Col><h3 className="display-4 fs-1 text-center mt-3">GENERAL</h3></Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ul className="px-5 mx-5">
+                            <li>Any member having a group of non-members of more than ten (10) should notify a member of the Board of Directors beforehand.</li>
+                            <li>Children may not use club unless there is a responsible member present.</li>
+                            <li>Guests are the responsibility of the member bringing them.</li>
+                            <li>Each member or group is responsible for leaving area used in as good or better condition than found. No one is paid to clean up after you. Do your part!</li>
+                            <li>No motorcycle or ATV riding on club grounds.</li>
+                            <li>No vehicles on the dam. (This is a STATE LAW.)</li>
+                            <li>No privately owned (permanent) buildings are allowed on club grounds.</li>
+                            <li>It is the responsibility of the Board of Directors and Officers along with all club members to ask any person, if not known that comes onto the club property, who they are and if they have a membership card. This is a good way to know your club members and also keep unauthorized people out. Please do not threaten or be confrontational with them. Refer to the membership list or contact the Secretary/Treasurer to verify their membership.</li>
+                            <li>The use of illegal drugs, excessive use of alcohol and profane language will not be tolerated. Members and or guests in violation of this club rule will be asked to leave the club premises. Membership could be suspended.</li>
+                            <li>All local and state open burning laws must be adhered to. Campfires or cooking fires are allowed only at a campsite enclosed by a fire ring or on the eastside of the clubhouse in the fire pit at the picnic area. Please do not leave fire unattended and extinguish fire before going home. Do not leave your trash in the fire. Take it home with you.</li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col><h3 className="display-4 fs-1 text-center mt-3">CLUBHOUSE RULES</h3></Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ul className="px-5 mx-5">
+                            <li>No clubhouse or porch furniture should be taken from the building.</li>
+                            <li>No cooking of any kind in the clubhouse or porch except in the kitchen on the electric stove or in the microwave.</li>
+                            <li>Leave the driveway around the clubhouse clear at all times. No parking in the road.</li>
+                            <li>No parking between clubhouse and the swimming area. No parking in the picnic area.</li>
+                            <li>No dogs allowed in the clubhouse or on the screened porch.</li>
+                            <li>All swimmers with wet feet should use the back door and stay out of the clubhouse. Use the shower or bathrooms for dressing.</li>
+                            <li>The clubhouse shall not be used as living quarters.</li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col><h3 className="display-4 fs-1 text-center mt-3">PICNIC AND CAMPING AREA</h3></Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ul className="px-5 mx-5">
+                            <li>Clean up area used. TAKE ALL GARBAGE HOME WITH YOU. Leave only paper in barrels and burn at once.</li>
+                            <li>Put aluminum cans in containers marked for them.</li>
+                            <li>Do not put cans or glass containers of any kind in the trash barrels. Take them home with you.</li>
+                            <li>All dogs must be kept on a leash at all times.</li>
+                            <li>No tables should be taken from the picnic area to a camping area. You must bring your own table for your campsite.</li>
+                            <li>No camping in the marked picnic area (around the clubhouse).</li>
+                            <li>All campsites with trailers must be used for camping, NOT FOR STORAGE. Keep the grass cut around your campsite.</li>
+                            <li>No air conditioners allowed.</li>
+                            <li>Only normal camping equipment show be kept on club property.</li>
+                            <li>Campers that are no longer being used on campsites unkept can be torn down and hauled away at the owner's expense following a two (2) week notification by mail. REMOVE YOUR CAMPER IMMEDIATELY IF YOU DO NOT INTEND TO USE IT FOR THE SEASON!</li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col><h3 className="display-4 fs-1 text-center mt-3">BEACH AREA AND BOAT DECK</h3></Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ul className="px-5 mx-5">
+                            <li className="fw-bold">NO LIFEGUARD IS ON DUTY.</li>
+                            <li>Children under 16 must have a responsible adult (18 years or older) supervising them at the beach while swimming or boating.</li>
+                            <li>No swimming outside the marked swimming area.</li>
+                            <li>No swimming from boats.</li>
+                            <li>No cans, bottles or food on beach or in swimming area.</li>
+                            <li>Keep boats out of swimming area.</li>
+                            <li>No dogs in swimming area.</li>
+                            <li>No fishing in swimming area.</li>
+                            <li>Fisherman have first choice of boats.</li>
+                            <li>Do not dump unused minnows in lake.</li>
+                            <li>Return all boats to the boat dock.  Store them upside down.</li>
+                            <li>No gasoline motors on lake.</li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col><h3 className="display-4 fs-1 text-center mt-3">SHOOTING RANGE</h3></Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ul className="px-5 mx-5">
+                            <li>All target practice should take place on the range only.</li>
+                            <li>Do not shoot at any target that is not backed by a backstop.</li>
+                            <li>Shooting of fireworks should be confined to the rifle range.</li>
+                            <li>Tannerite explosive targets are NOT ALLOWED to be used on the 80 property.</li>
+                            <li>RECOVER ALL TARGETS (CARDBOARD, CANS, ETC) WHEN FINISHED ON THE RANGE.</li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col><h3 className="display-4 fs-1 text-center mt-3">HUNTING</h3></Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ul className="px-5 mx-5">
+                            <li>All hunting is for members only (no guests).  Deer hunting bow only, no guns.</li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col><p className="text-muted">NOTE: The Board of Directors has the right to suspend a member or members at their discretion if it is brought to the Board's attention that a rule or By-Law of the club has been violated. The member or members will be notified either verbally, by phone, or by mail of their suspension. If they desire to appeal the suspension, the Board will notify them at the Board's next meeting.</p></Col>
                 </Row>
             </Container>
         </SidebarWithContent>
